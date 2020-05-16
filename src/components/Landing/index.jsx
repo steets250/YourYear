19804564d2a8896926b4d04c4df5ad5a059a6625
bamkho4 <phanmichael77@gmail.com>
2020-05-16 04:00:01 -0700

    Login Page

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './style.less';
 
+import Picture from '../../landing.png';
+
 const Landing = () => {
     return (
         <div className="landing">
             <p>landing component</p>
+            <img src={Picture} style={{height: 200, width: 200}}></img>
         </div>
     );
 };
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { PageHeader } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import { Button, Radio } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
 
 import './style.less';
 
 const Login = () => {
     return (
         <div className="login">
-            <p>login component</p>
+            <div className="header"> 
+                <div className="header-title">YourYear</div>
+            </div>
+            <div className="body">
+                {/* <div className="square"></div> */}
+                <div className="welcome">Member Login</div>
+                <div className="input-container">
+                    <Input className="input" size="large" placeholder="Username" />
+                </div>
+                <div className="input-container"> 
+                        <Input className="input" size="large" placeholder="Password" />
+                </div>
+                <div className="button-container">
+                    <Button className="button" type="primary" shape="round" size={"large"}>
+                    Login
+                    </Button>
+                 </div>
+            </div>
         </div>
     );
 };
