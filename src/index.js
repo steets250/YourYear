836f01ea23a769a7971426dcb69315218984e679
Steven Steiner <steets250@gmail.com>
2020-05-16 02:47:47 -0700

    WebFonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import GuestPage from './containers/GuestPage';
@@ -7,6 +8,15 @@ import LandingPage from './containers/LandingPage';
 import LoginPage from './containers/LoginPage';
 import OnboardPage from './containers/OnboardPage';
 import RegisterPage from './containers/RegisterPage';
+
+WebFont.load({
+  google: {
+    families: [
+      'Raleway:100,200,300,400,500,600,700,800,900',
+      'Montserrat:100,200,300,400,500,600,700,800,900',
+    ],
+  },
+});
 
 const App = () => {
   return (
