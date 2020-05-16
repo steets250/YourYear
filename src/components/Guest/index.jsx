985d0fe6df32b6aeb13b7c7188b42d6c9b6b9232
bamkho4 <phanmichael77@gmail.com>
2020-05-16 02:55:13 -0700

    Guest Page

--- src/components/Guest/index.jsx
+++ src/components/Guest/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { PageHeader } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import { Button, Radio } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
 
 import './style.less';
 
 const Guest = () => {
     return (
         <div className="guest">
-            <p>guest component</p>
+            <div className="header"> 
+                <div className="header-title">YourYear</div>
+            </div>
+            <div className="body">
+                <div className="welcome">Your year starts now!</div>
+                <div className="input-container">
+                    <Input className="input" size="large" placeholder="What is your name?" />
+                </div>
+                <div className="button-container">
+                    <Button className="button" type="primary" shape="round" size={"large"}>
+                    Get Started
+                    </Button>
+                 </div>
+            </div>
         </div>
     );
 };
