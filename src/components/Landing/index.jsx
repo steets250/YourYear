802c92d0b88eb5e0987ab209b9305142ba6b3d5d
Steven Steiner <steets250@gmail.com>
2020-05-17 04:07:54 -0700

    Text Change

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -19,7 +19,7 @@ const Landing = () => {
 
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw' }}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '50%' }}>
-                        <h1>A 4-year course planner made with your way</h1>
+                        <h1>A 4-year course planner made just for you</h1>
                         <h2>YourYear allows students to plan their educational plan with a sleek drag and drop interface. We make sure you reach you fulfill all university requirements and as well as keeping track of your GPA and units.</h2>
                     </div>
                 </div>
