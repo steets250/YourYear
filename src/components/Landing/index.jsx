72d364e4f8b3f5b3c076f5ba21bd1c4240e85f46
bamkho4 <phanmichael77@gmail.com>
2020-05-16 21:47:52 -0700

    landing page header

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { Button, Radio } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 import './style.less';
 
 import Picture from '../../landing.png';
@@ -7,8 +8,16 @@ import Picture from '../../landing.png';
 const Landing = () => {
     return (
         <div className="landing">
-            <p>landing component</p>
-            <img src={Picture} style={{height: 200, width: 200}}></img>
+            <div className="header">
+                <div className="header-title">Your Year</div>
+                <div className="button-container">
+                    <Button className="signin-button" type="primary" shape="round" size={"large"}>
+                    Login
+                    </Button>
+                </div>
+                <div className="about-us">About us</div>
+            </div>
+            {/* <img src={Picture} style={{ height: 200, width: 200 }}></img> */}
         </div>
     );
 };
