525d4a03ab664e126dcefc61eea1399b0e9f6596
Steven Steiner <steets250@gmail.com>
2020-05-17 02:35:38 -0700

    Clean Login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -11,18 +11,20 @@ const Login = () => {
             <div className="header">
                 <div className="header-title">YourYear</div>
             </div>
-            <div className="body">
-                <div className="welcome">Member Login</div>
-                <div className="input-container">
-                    <Input className="input" size="large" placeholder="Username" />
-                </div>
-                <div className="input-container">
-                    <Input className="input" size="large" placeholder="Password" />
-                </div>
-                <div className="button-container">
-                    <Button className="button" type="primary" shape="round" size={"large"} onClick={() => {history.push('/onboard')}}>
-                        Login
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '80%'}}>
+                <div className="body">
+                    <div className="welcome">Member Login</div>
+                    <div className="input-container">
+                        <Input className="input" size="large" placeholder="Username" />
+                    </div>
+                    <div className="input-container">
+                        <Input className="input" size="large" placeholder="Password" />
+                    </div>
+                    <div className="button-container">
+                        <Button className="button" type="primary" shape="round" size={"large"} onClick={() => { history.push('/onboard') }}>
+                            Login
                     </Button>
+                    </div>
                 </div>
             </div>
         </div>
